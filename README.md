18b6ea790b18d2a55fccc5de388916ae3b3e0c95
Mstislav Zhivodkov <stevemyz@gmail.com>
2017-12-01 17:21:13 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Typescript Boilerplate [![Build Status](https://travis-ci.org/Trufi/typescript-boilerplate.svg?branch=master)](https://travis-ci.org/Trufi/typescript-boilerplate)
+
+Boilerplate for projects with typescript and webpack.
+
+[Demo](https://trufi.github.io/typescript-boilerplate/)
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = (env = {}) => {
+    const tsCheckerPlugin = new ForkTsCheckerWebpackPlugin({
+        watch: ['./src'],
+    });
+
+    const plugins = [tsCheckerPlugin];
+
+    if (env.producton) {
+        plugins.push(new webpack.optimize.UglifyJsPlugin({sourceMap: true}));
+    }
+
+    const config = {
+        module: {
+            rules: [{
+                test: /(\.ts|\.tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                    },
+                },
+            }],
+        },
+
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+
+        entry: './src/index.ts',
+
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/',
+        },
+
+        plugins,
+
+        devtool: 'source-map',
+
+        devServer: {
+            host: '0.0.0.0',
+            port: 3000,
+            stats: {
+                modules: false,
+            },
+            disableHostCheck: true,
+        }
+    };
+
+    return config;
+};
