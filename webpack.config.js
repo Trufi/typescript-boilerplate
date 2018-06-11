2877910bd9ae42d13ac8328546aa66d07192c169
Mstislav Zhivodkov <stevemyz@gmail.com>
2018-06-11 22:48:15 +0700

    Update to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -2,56 +2,54 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = (env = {}) => {
-    const tsCheckerPlugin = new ForkTsCheckerWebpackPlugin({
-        watch: ['./src'],
-    });
+const production = process.env.NODE_ENV === 'production';
 
-    const plugins = [tsCheckerPlugin];
+const tsCheckerPlugin = new ForkTsCheckerWebpackPlugin({
+    watch: ['./src'],
+});
 
-    if (env.production) {
-        plugins.push(new webpack.optimize.UglifyJsPlugin({sourceMap: true}));
-    }
+const plugins = [tsCheckerPlugin];
 
-    const config = {
-        module: {
-            rules: [{
-                test: /(\.ts|\.tsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'ts-loader',
-                    options: {
-                        transpileOnly: true,
-                    },
+if (production) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({sourceMap: true}));
+}
+
+module.exports = {
+    module: {
+        rules: [{
+            test: /(\.ts|\.tsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true,
                 },
-            }],
-        },
-
-        resolve: {
-            extensions: ['.ts', '.js'],
-        },
-
-        entry: './src/index.ts',
-
-        output: {
-            filename: 'index.js',
-            path: path.resolve(__dirname, 'dist'),
-            publicPath: '/dist',
-        },
-
-        plugins,
-
-        devtool: 'source-map',
-
-        devServer: {
-            host: '0.0.0.0',
-            port: 3000,
-            stats: {
-                modules: false,
             },
-            disableHostCheck: true,
-        }
-    };
+        }],
+    },
 
-    return config;
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+
+    entry: './src/index.ts',
+
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist',
+    },
+
+    plugins,
+
+    devtool: 'source-map',
+
+    devServer: {
+        host: '0.0.0.0',
+        port: 3000,
+        stats: {
+            modules: false,
+        },
+        disableHostCheck: true,
+    }
 };
