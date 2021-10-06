c2c2ce7256f114cd17a4218eb94fdf7a6c2c0d3d
Mstislav Zhivodkov <stevemyz@gmail.com>
2021-10-06 22:21:57 +0700

    Disable webpack overlay

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,9 @@ module.exports = (env) => {
             host: '0.0.0.0',
             port: 3000,
             allowedHosts: 'all',
+            client: {
+                overlay: false,
+            },
         },
     };
 };
